23304f854e6b9efc0741d545334e398c5c08c92d
“x-raf <x-raf@wp.pl>
2018-09-23 05:48:50 +0200

    Last refactor v2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const apiRoot = 'https://gentle-escarpment-91656.herokuapp.com/v1/';
+  const trelloApiRoot = 'https://gentle-escarpment-91656.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
